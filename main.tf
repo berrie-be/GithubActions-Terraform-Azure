6e75d3e253bc5da9a12cb56459a69c2f0d30804e
Kaberi Agarwala <82584598+berrie-be@users.noreply.github.com>
2026-02-19 18:26:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,22 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.7.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "practice-github-actions-terraform"
+    storage_account_name = "githubactionsterraformm"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+    use_oidc             = true
+  }
+}
+
+provider "azurerm" {
+  features {}
+  use_oidc = true
+}
 
